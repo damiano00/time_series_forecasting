50fe17964511c503e3661dce9408f0d64cf36e16
damiano00 <pasquini.damiano00@gmail.com>
2025-02-24 17:04:48 +0100

    [edit] data_processor.py, run.py, utils.py

--- src/data_processor.py
+++ src/data_processor.py
@@ -1,76 +1,99 @@
+import datetime
 import os
 import torch
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
-from torch.utils.data import DataLoader, TensorDataset
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from torch.utils.data import DataLoader, TensorDataset
+
 
 class DataProcessor:
-
-    def __init__(self, dir_path, seq_len, test_size, cols_to_scale, cols_to_normalize, batch_size, feature_columns, label_columns):
+    def __init__(self, dir_path: str, seq_len: int, test_size: float, cols_to_normalize: list[str], batch_size: int,
+                 smoothing_window_size: int, feature_columns: list[str], label_columns: list[str]):
         self.dir_path = dir_path
         self.df = None
         self.seq_len = seq_len
         self.batch_size = batch_size
+        self.smoothing_window_size = smoothing_window_size
         self.train_size = 1 - test_size
         self.test_size = test_size
         self.train_data = None
         self.test_data = None
-        self.cols_to_scale = cols_to_scale
         self.cols_to_normalize = cols_to_normalize
         self.feature_columns = feature_columns
         self.label_columns = label_columns
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
 
-    def split_df(self):
-        if self.df is None:
-            raise ValueError("The dataframe is empty.")
-        self.train_data = self.df[:int(len(self.df) * self.train_size)]
-        self.test_data = self.df[int(len(self.df) * self.train_size):]
-
-    def csv_to_df(self, stocks_list):
-        self.df = pd.DataFrame()
+    def load_csv(self, stocks_list: list[str], make_date_index: bool = True):
+        """
+        Read the CSV files and concatenate them into a single dataframe.
+        :param stocks_list: List of stock CSV files
+        :param make_date_index: Whether to set the 'Date' column as the index
+        """
+        df_list = []
         for stock in stocks_list:
             stock_data = pd.read_csv(os.path.join(self.dir_path, stock) if self.dir_path else stock)
-            self.df = pd.concat([self.df, stock_data])
+            df_list.append(stock_data)
+
+        self.df = pd.concat(df_list, ignore_index=True)
         self.df.replace([np.inf, -np.inf], np.nan, inplace=True)
         self.df.ffill(inplace=True)
-        self.df.sort_values(by='Date', inplace=True)
-        self.transform_dates(self.df)
+        self.df.bfill(inplace=True)  # Backward fill as well
+
+        if "Date" in self.df.columns:
+            self.df["Date"] = pd.to_datetime(self.df["Date"], errors='coerce')
+            self.df.dropna(subset=["Date"], inplace=True)
+            self.df.sort_values(by="Date", inplace=True)
+            if make_date_index:
+                self.df.set_index("Date", inplace=True)
+
         self.feature_columns = [col for col in self.df.columns if col not in self.label_columns]
 
-    @staticmethod
-    def transform_dates(df):
-        df["Date"] = pd.to_datetime(df["Date"])
-        df.reset_index(drop=True, inplace=True)
-        df['month_sin'] = np.sin(2 * np.pi * df['Date'].dt.month / 12)
-        df['month_cos'] = np.cos(2 * np.pi * df['Date'].dt.month / 12)
-        df['day_sin'] = np.sin(2 * np.pi * df['Date'].dt.day / 31)
-        df['day_cos'] = np.cos(2 * np.pi * df['Date'].dt.day / 31)
-        df['weekday'] = df['Date'].dt.weekday
-        df.drop(columns=['Date'], inplace=True)
+    def split_df(self):
+        """Split the dataframe into training and testing sets."""
+        if self.df is None or self.df.empty:
+            raise ValueError("The dataframe is empty.")
+        split_index = int(len(self.df) * self.train_size)
+        self.train_data = self.df.iloc[:split_index].copy()
+        self.test_data = self.df.iloc[split_index:].copy()
 
-    def feature_transformation(self, df):
-        if self.cols_to_scale:
-            scaler = StandardScaler()
-            for col in self.cols_to_scale:
-                if col in df.columns:
-                    df[col] = scaler.fit_transform(df[col].values.reshape(-1, 1))
-        if self.cols_to_normalize:
-            scaler = MinMaxScaler()
-            for col in self.cols_to_normalize:
-                if col in df.columns:
-                    df[col] = scaler.fit_transform(df[col].values.reshape(-1, 1))
+    def normalize_features(self):
+        """Normalize specified columns using MinMaxScaler."""
+        if self.train_data is None or self.test_data is None:
+            raise ValueError("Data not split. Run `split_df()` first.")
 
-    def _create_dataset(self, dataset):
+        missing_cols = [col for col in self.cols_to_normalize if col not in self.train_data.columns]
+        if missing_cols:
+            raise ValueError(f"Missing columns for normalization: {missing_cols}")
+
+        self.scaler.fit(self.train_data[self.cols_to_normalize])
+
+        self.train_data[self.cols_to_normalize] = self.scaler.transform(self.train_data[self.cols_to_normalize])
+        self.test_data[self.cols_to_normalize] = self.scaler.transform(self.test_data[self.cols_to_normalize])
+
+        print("Feature normalization complete.")
+
+    def _create_sequences(self, dataset: pd.DataFrame):
+        """Create sequences for LSTM input."""
         X, y = [], []
+        dataset_values = dataset[self.feature_columns].values
+        label_values = dataset[self.label_columns].values
+
         for i in range(len(dataset) - self.seq_len):
-            X.append(dataset[self.feature_columns].iloc[i:(i + self.seq_len)].values)
-            y.append(dataset[self.label_columns].iloc[i + self.seq_len].values)
+            X.append(dataset_values[i:(i + self.seq_len)])
+            y.append(label_values[i + self.seq_len])
+
         return np.array(X), np.array(y)
 
-    def get_data_loader(self, is_train=True):
-        X_train, y_train = self._create_dataset(self.train_data if is_train else self.test_data)
-        X_train = torch.tensor(X_train, dtype=torch.float32)
-        y_train = torch.tensor(y_train, dtype=torch.float32)
-        train_dataset = TensorDataset(X_train, y_train)
-        return DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True)
+    def get_data_loader(self, is_train: bool = True):
+        """Create a PyTorch DataLoader."""
+        dataset = self.train_data if is_train else self.test_data
+        if dataset is None:
+            raise ValueError("Data has not been split. Run `split_df()` first.")
+
+        X_data, y_data = self._create_sequences(dataset)
+        X_tensor = torch.tensor(X_data, dtype=torch.float32)
+        y_tensor = torch.tensor(y_data, dtype=torch.float32)
+        dataset_tensor = TensorDataset(X_tensor, y_tensor)
+
+        return DataLoader(dataset_tensor, batch_size=self.batch_size, shuffle=False)
--- src/run.py
+++ src/run.py
@@ -1,70 +1,48 @@
 import os
 import torch
+import matplotlib.pyplot as plt
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from src.data_processor import DataProcessor
 from src.lstm.lstm import LSTM
 from src.utils import Environment
+from datetime import datetime
+
+
+current_time = datetime.now().strftime("%Y%m%d%H")
 
 
 def save_model_checkpoint(model, epoch, result_dir):
-    if (epoch + 1) % 10 == 0:
+    if (epoch + 1) % 50 == 0:
         checkpoint_path = f"{result_dir}/model_checkpoint_epoch_{epoch + 1}.pth"
         torch.save(model.state_dict(), checkpoint_path)
 
 
-def calculate_metrics(test_loader, model):
-    y_true = torch.cat([y for _, y in test_loader], dim=0)
-    y_pred = torch.cat([model(X).detach() for X, _ in test_loader], dim=0)
-
+def calculate_metrics(test_loader, model, device):
+    y_true, y_pred = [], []
+    model.eval()
+    with torch.no_grad():
+        for X_batch, y_batch in test_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            predictions = model(X_batch)
+            y_true.append(y_batch)
+            y_pred.append(predictions)
+    y_true = torch.cat(y_true, dim=0).cpu().numpy()
+    y_pred = torch.cat(y_pred, dim=0).cpu().numpy()
     mae = mean_absolute_error(y_true, y_pred)
     mse = mean_squared_error(y_true, y_pred)
     r2 = r2_score(y_true, y_pred)
-
     return mae, mse, r2
 
 
 def save_metrics(metrics_dir, mae, mse, r2):
     os.makedirs(metrics_dir, exist_ok=True)
     with open(f"{metrics_dir}/metrics.txt", "w") as f:
-        f.write(f"MAE: {mae}\n")
-        f.write(f"MSE: {mse}\n")
-        f.write(f"R2 Score: {r2}\n")
+        f.write(f"MAE: {mae}\nMSE: {mse}\nR2 Score: {r2}\n")
 
 
-def train_and_validate(model, criterion, optimizer, train_loader, test_loader, num_epochs, result_dir, test_stock_name):
-    train_losses = []
-    test_losses = []
-
-    for epoch in range(num_epochs):
-        try:
-            # Training Phase
-            model.train()
-            for X_train_batch, y_train_batch in train_loader:
-                loss = train_step(model, criterion, optimizer, X_train_batch, y_train_batch)
-                train_losses.append(loss)
-
-            # Validation Phase
-            val_loss = validate_step(model, criterion, test_loader)
-            test_losses.append(val_loss)
-
-            # Save model checkpoint every 10 epochs
-            save_model_checkpoint(model, epoch, result_dir)
-
-            print(
-                f'Epoch [{epoch + 1}/{num_epochs}], Train Loss: {train_losses[-1]:.4f}, Validation Loss: {val_loss:.4f}')
-
-        except KeyboardInterrupt:
-            print("\nTraining interrupted. Saving model...")
-            torch.save(model.state_dict(), f"{result_dir}/model_interrupted.pth")
-            break
-
-    # After training, calculate and save metrics
-    mae, mse, r2 = calculate_metrics(test_loader, model)
-    metrics_dir = f"{result_dir}/{test_stock_name}"
-    save_metrics(metrics_dir, mae, mse, r2)
-
-
-def train_step(model, criterion, optimizer, X_batch, y_batch):
+def train_step(model, criterion, optimizer, X_batch, y_batch, device):
+    model.train()
+    X_batch, y_batch = X_batch.to(device), y_batch.to(device)
     optimizer.zero_grad()
     outputs = model(X_batch)
     loss = criterion(outputs, y_batch)
@@ -73,14 +51,37 @@ def train_step(model, criterion, optimizer, X_batch, y_batch):
     return loss.item()
 
 
-def validate_step(model, criterion, test_loader):
+def validate_step(model, criterion, test_loader, device):
     model.eval()
-    val_loss = 0.0
+    total_loss = 0.0
     with torch.no_grad():
-        for X_test_batch, y_test_batch in test_loader:
-            val_outputs = model(X_test_batch)
-            val_loss += criterion(val_outputs, y_test_batch).item()
-    return val_loss / len(test_loader)
+        for X_batch, y_batch in test_loader:
+            X_batch, y_batch = X_batch.to(device), y_batch.to(device)
+            outputs = model(X_batch)
+            total_loss += criterion(outputs, y_batch).item()
+    return total_loss / len(test_loader)
+
+
+def train_and_validate(model, criterion, optimizer, train_loader, test_loader, num_epochs, result_dir, test_stock_name,
+                       device):
+    for epoch in range(num_epochs):
+        try:
+            epoch_train_losses = []
+            for X_batch, y_batch in train_loader:
+                loss = train_step(model, criterion, optimizer, X_batch, y_batch, device)
+                epoch_train_losses.append(loss)
+            train_loss = sum(epoch_train_losses) / len(epoch_train_losses)
+            val_loss = validate_step(model, criterion, test_loader, device)
+            save_model_checkpoint(model, epoch, result_dir)
+            print(f'Epoch [{epoch + 1}/{num_epochs}], Train Loss: {train_loss:.4f}, Validation Loss: {val_loss:.4f}')
+        except KeyboardInterrupt:
+            print("\nTraining interrupted. Saving model...")
+            torch.save(model.state_dict(), f"{result_dir}/model_interrupted.pth")
+            break
+
+    mae, mse, r2 = calculate_metrics(test_loader, model, device)
+    metrics_dir = f"{result_dir}/{test_stock_name}"
+    save_metrics(metrics_dir, mae, mse, r2)
 
 
 def get_stock_names(num_csvs):
@@ -94,6 +95,7 @@ def get_stock_names(num_csvs):
                 'GE.csv', 'INTC.csv', 'MSFT.csv', 'nvda.csv', 'pypl.csv', 'QQQ.csv', 'SBUX.csv', 'T.csv', 'TSLA.csv',
                 'WFC.csv', 'KO.csv', 'AMD.csv', 'TSM.csv', 'GOOG.csv', 'WMT.csv']
     names_5 = ['KO.csv', 'AMD.csv', 'TSM.csv', 'GOOG.csv', 'WMT.csv']
+    names_1 = ['AMD.csv']
 
     if num_csvs == 5:
         return names_5
@@ -101,78 +103,70 @@ def get_stock_names(num_csvs):
         return names_25
     elif num_csvs == 50:
         return names_50
+    elif num_csvs == 1:
+        return names_1
     else:
-        raise ValueError("[Warning] The number of CSVs is invalid, please choose from 5, 25, or 50.")
+        raise ValueError("Invalid number of CSVs; choose from 1, 5, 25, or 50.")
 
 
-def main(data_path, num_csvs, cols_to_scale, cols_to_normalize, test_stock_name, num_epochs, seq_len,
-         batch_size, dropout, feature_columns, label_columns, test_size, hidden_size, num_layers, learning_rate,
-         use_gpu=True):
-    # Directories for saving results
+def main(data_path, num_csvs, cols_to_normalize, test_stock_name, epochs, seq_len, batch_size, dropout, feature_columns,
+         label_columns, test_size, smoothing_window_size, hidden_size, num_layers, learning_rate, use_gpu):
+
     results_path = f"../src/lstm/test_result_{num_csvs}"
     os.makedirs(results_path, exist_ok=True)
 
-    # Data Processor
     dp = DataProcessor(
         dir_path=data_path,
         seq_len=seq_len,
         test_size=test_size,
-        cols_to_scale=cols_to_scale,
         cols_to_normalize=cols_to_normalize,
         batch_size=batch_size,
+        smoothing_window_size=smoothing_window_size,
         feature_columns=feature_columns,
         label_columns=label_columns
     )
 
-    # Define test stock names based on num_csvs
     stock_names = get_stock_names(num_csvs)
 
-    # Check CUDA availability
-    Environment.check_cuda(self=Environment(), use_gpu=use_gpu)
+    env = Environment(use_gpu=use_gpu)
+    env.check_cuda()
 
-    # Prepare data
-    dp.csv_to_df(stocks_list=stock_names)
+    # Use the correct method names: load_csv and normalize_features.
+    dp.load_csv(stocks_list=stock_names)
     dp.split_df()
-    dp.feature_transformation(dp.train_data)
-    dp.feature_transformation(dp.test_data)
+    dp.normalize_features()
 
-    # Get data loaders
-    train_loader = dp.get_data_loader(True)
-    test_loader = dp.get_data_loader(False)
+    train_loader = dp.get_data_loader(is_train=True)
+    test_loader = dp.get_data_loader(is_train=False)
 
-    # Model parameters
     input_size = next(iter(train_loader))[0].shape[2]
     output_size = next(iter(train_loader))[1].shape[1]
 
-    # Model, Loss, Optimizer
     model = LSTM(input_size, hidden_size, num_layers, output_size, dropout)
-    criterion = torch.nn.MSELoss()  # Mean Squared Error Loss
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  # Adam optimizer
+    device = torch.device("cuda" if use_gpu and torch.cuda.is_available() else "cpu")
+    model.to(device)
 
-    # Training and validation
-    train_and_validate(model, criterion, optimizer, train_loader, test_loader, num_epochs, results_path,
-                       test_stock_name=test_stock_name)
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-
-
+    train_and_validate(model, criterion, optimizer, train_loader, test_loader, epochs, results_path, test_stock_name,
+                       device)
 
 
 if __name__ == "__main__":
     main("../dataset/processed/data_for_lstm",
          5,
-         [1, 2, 3, 4, 5, 6],
-         [1, 2, 3, 4, 5, 6],
+         ['Open', 'High', 'Low', 'Volume', 'Sentiment_gpt', 'News_flag', 'Scaled_sentiment'],
          test_stock_name='GOOG',
          dropout=0.3,
-         num_epochs=2000,
+         epochs=200,
          seq_len=60,
-         batch_size=32,
-         feature_columns=['Open', 'High', 'Low', 'Volume', 'Sentiment_gpt', 'News_flag', 'Scaled_sentiment',
-                          'month_sin', 'month_cos', 'day_sin', 'day_cos', 'weekday'],
-         label_columns=['Close', 'Adj close'],
+         batch_size=64,
+         feature_columns=['Open', 'High', 'Low', 'Volume', 'Sentiment_gpt', 'News_flag', 'Scaled_sentiment'],
+         label_columns=['Close'],
          test_size=0.2,
+         smoothing_window_size=2500,
          learning_rate=0.001,
          hidden_size=64,
          num_layers=2,
          use_gpu=True)
-
